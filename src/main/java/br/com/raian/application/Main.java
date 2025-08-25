2232ae665b20af6d062f64ff44a7dd7bd7ca0c52
Raian Emanuel Uchôa Laurentino <raian.pro9@gmail.com>
2025-08-25 15:32:38 -0300

    Reusing instantiation

--- src/main/java/br/com/raian/application/Main.java
+++ src/main/java/br/com/raian/application/Main.java
@@ -10,8 +10,8 @@ public class Main {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("==== TEST 1: Seller findById ====");
         Seller seller = sellerDao.findById(3);
-
-        System.out.println(seller   );
+        System.out.println(seller);
     }
 }
