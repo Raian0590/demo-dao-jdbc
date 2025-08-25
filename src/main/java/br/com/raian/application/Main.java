1225a96a43886cde4c0c2b6181faee0e46fae912
Raian Emanuel Uchôa Laurentino <raian.pro9@gmail.com>
2025-08-25 20:43:49 -0300

    insert implementation

--- src/main/java/br/com/raian/application/Main.java
+++ src/main/java/br/com/raian/application/Main.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -29,6 +30,11 @@ public class Main {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n==== TEST 4: Seller insert ====");
+        Seller seller1 = new Seller(null, "Greg", "greg@gmail.com", new Date(), 1000.0, department);
+        sellerDao.insert(seller1);
+        System.out.println("Inserted! New id: " + seller1.getId());
 
     }
 }
